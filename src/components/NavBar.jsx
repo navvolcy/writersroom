0a0beb92ace82e78a10d7d5638b458793beaacd0
Nav L. Volcy <45085887+navvolcy@users.noreply.github.com>
2024-04-18 14:53:31 -0600

    Tailwindcss has been configured to react

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-scroll';
+
+
+const NavBar = () => {
+    return(
+        <div className="main-container">NavBar</div>
+    )
+
+}
+
+export default NavBar;
