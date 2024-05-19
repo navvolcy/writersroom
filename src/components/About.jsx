0ee885e7236953d6ef5e43fcdce9d322ed8567e7
Nav Volcy <navvolcy@gmail.com>
2024-05-18 18:22:04 -0600

    worked on individual pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,19 @@ import profile from './../assets/Navi.JPG';
 
 const About  = () => {
     return(
-    <div className="items-center gap-x-10 p-10 bg-repeat">
-        <div className="object-cover bg-orange-400 p-10">
+    <div className="items-center gap-x-10 p-10 ">
+        <div className="grid sm:grid-cols-2 object-cover bg-orange-400 p-10">
+            <div>
             <img  src={profile} alt="profile pic"/> 
+            </div>
+            <div className="sm:text-left text-4xl  italic">
+                <p>
+                    Hello  world
+                </p>
+                
+            </div>
         </div>  
+
     </div>
 
     )
--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Episodes = () => {
     return (
         <div>
-            Episodes
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div className="w-full flex justify-center items-center flex-col mb-7">
+                    <p className='text-4xl  italic inline border-b-4 border-cyan-500 text-center'>Episodes</p>
+                    <p  className="py-4 text-2xl">Writer's Room </p>
+                </div>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8"></div>
+
+                </div>
         </div>
     )
 }
