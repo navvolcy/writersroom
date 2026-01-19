cc63ded22a6fd1bf8737e78ebb5b1b5f2c5dd673
Nav Volcy <navvolcy@gmail.com>
2026-01-18 21:56:12 -0700

    model for episodes

--- server/src/models/Episode.js
+++ server/src/models/Episode.js
@@ -1,6 +1,50 @@
 // how data looks in MongoDB fro user login ,epidsode info. favorites list for each individual account.
 import mongoose from 'mongoose';
 
-const EpisodeSchema = new mongoose.Schema({
+const EpisodeSchema = new mongoose.Schema(
+    {
+        episodeNumber: {
+            type: Number,
+            required:true,
+            unique:true,
+            min:1,
+        },
 
-})
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength:120,
+        },
+
+         description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    airDate: {
+      type: Date,
+      required: true,
+    },
+
+    durationMinutes: {
+      type: Number,
+      min: 1,
+    },
+
+    guests: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('Episode', EpisodeSchema);
+        
+
+
+
