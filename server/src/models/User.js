84e00c39a106b6ba496f12f1b41eeecf2f150e6f
Nav Volcy <navvolcy@gmail.com>
2026-01-21 18:34:53 -0700

    updated

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema(
     {
@@ -30,5 +31,20 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
     }
 );
+
+
+//Hash password before saving
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//Compare entered password with stored hash
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model('User', UserSchema);
