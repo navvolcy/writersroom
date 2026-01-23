3513374b346d71bee07851844a164ca822c7fa45
Nav Volcy <navvolcy@gmail.com>
2026-01-22 17:17:05 -0700

    added just a few comments for bycrypt

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -37,12 +37,12 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10);//generates random string of chars & preforms 10 rounds of hashing
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-//Compare entered password with stored hash
+//Compare entered password with stored hash (Hash Verification)
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
