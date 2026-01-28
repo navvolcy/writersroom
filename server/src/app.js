3861887d3fdebbb5843cfb16209ec53f525c9e43
Nav Volcy <navvolcy@gmail.com>
2026-01-28 14:12:20 -0700

    Initial Writer's Room app

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', "https://YOUR-FRONTEND.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
