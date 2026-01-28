1563b718971b95a8c1a59e135d0462b6baf233f4
Nav Volcy <navvolcy@gmail.com>
2026-01-27 19:52:33 -0700

    fixed app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,14 @@
+import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+
+const app = express();
+
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+export default app;
