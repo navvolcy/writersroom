06e26bc9872b27be17cce4b3c9f9fab0179dc3f8
Nav Volcy <navvolcy@gmail.com>
2026-01-27 20:22:18 -0700

    tested routes in postman

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,18 @@ import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
+//middleware
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+//test route
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
+});
+
+app.post('/api/auth/test', (req, res) => {
+  res.json({ message: 'Auth route works' });
 });
 
 export default app;
