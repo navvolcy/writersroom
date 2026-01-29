a313bdf62cc2a065d2b3d7a16858b7a52fd22288
Nav Volcy <navvolcy@gmail.com>
2026-01-28 18:12:16 -0700

    connected to mongo

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import cors from 'cors';
+import connectDB from "./config/db.js";
 
 const app = express();
+connectDB();
 
 app.use(cors({
   origin: [
