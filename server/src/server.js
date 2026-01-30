ca012423769b49fa6e671c2dfa6a1e63e36bb821
Nav Volcy <navvolcy@gmail.com>
2026-01-29 18:32:47 -0700

    patched

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
