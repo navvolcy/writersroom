f1f6e1b3f8c4e9308263bbc8b1206874a842d6c7
Nav Volcy <navvolcy@gmail.com>
2026-02-01 10:13:20 -0700

    removed next()

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
-  next();
+  
 });
 
 export default mongoose.model("User", UserSchema);
